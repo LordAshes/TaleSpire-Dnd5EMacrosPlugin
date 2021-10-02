c86833785da4e5cca5476868f1e73690ffc91bec
Lord Ashes <webtable@gmail.com>
2021-10-02 12:09:03 -0400

    Initial commit
    Implements attacks

---
+++ TaleSpireDnD5EMacroPlugin/DnD5EMacroPlugin.cs
@@ -0,0 +1,105 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Bounce.Unmanaged;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LordAshes
+{
+    [BepInPlugin(Guid, Name, Version)]
+    [BepInDependency(LordAshes.FileAccessPlugin.Guid)]
+    [BepInDependency(LordAshes.StatMessaging.Guid)]
+    [BepInDependency(RadialUI.RadialUIPlugin.Guid)]
+    public partial class DnD5EMacrosPlugin : BaseUnityPlugin
+    {
+        // Plugin info
+        public const string Name = "Dnd5e Macros Plug-In";
+        public const string Guid = "org.lordashes.plugins.dnd5emacros";
+        public const string Version = "1.0.0.0";
+
+        public Dictionary<string, Character> characters = new Dictionary<string, Character>();
+
+        private List<Tuple<NGuid, string>> messageQueue = new List<Tuple<NGuid, string>>();
+        private DateTime lastMessage = DateTime.UtcNow;
+        private int messageDelay = 1000;
+        private int lengthenMessage = 0;
+        private int expansionFontSize = 16;
+
+        /// <summary>
+        /// Function for initializing plugin
+        /// This function is called once by TaleSpire
+        /// </summary>
+        void Awake()
+        {
+            UnityEngine.Debug.Log("DnD5e Macros Plugin: Active.");
+
+            messageDelay = Config.Bind("Settings", "Message Delay", 1000).Value;
+            lengthenMessage = Config.Bind("Settings", "Increase Message Duration", 0).Value;
+            expansionFontSize = Config.Bind("Settings", "Chat Expension Font Size", 16).Value;
+
+            CampaignSessionManager.OnStatNamesChange += StatChange;
+
+            RadialUI.RadialUIPlugin.RemoveOnCharacter("Attacks");
+
+            RadialUI.RadialSubmenu.EnsureMainMenuItem(DnD5EMacrosPlugin.Guid + ".Attacks", RadialUI.RadialSubmenu.MenuType.character, "Attacks", FileAccessPlugin.Image.LoadSprite("Attack.png"));
+
+            foreach (string item in FileAccessPlugin.File.Find(".Dnd5e"))
+            {
+                string characterName = System.IO.Path.GetFileNameWithoutExtension(item);
+                Debug.Log("D&D 5e Macros Plug-In: Loading Character '" + characterName + "'");
+                characters.Add(characterName,JsonConvert.DeserializeObject<Character>(FileAccessPlugin.File.ReadAllText(item)));
+
+                foreach(Roll roll in characters[characterName].attacks)
+                {
+
+                    Debug.Log("D&D 5e Macros Plug-In: Adding Character '" + characterName + "' Roll '" + roll.name + "'");
+
+                    RadialUI.RadialSubmenu.CreateSubMenuItem(
+                                                                DnD5EMacrosPlugin.Guid + ".Attacks",
+                                                                roll.name,
+                                                                FileAccessPlugin.Image.LoadSprite(roll.type+".png"),
+                                                                (cid, obj, mi) => { MenuSelection(cid, roll); },
+                                                                true,
+                                                                () => { return CharacterCheck(characterName, roll.name); }
+                                                            );
+
+                }
+            }
+
+            Utility.PostOnMainPage(this.GetType());
+        }
+
+        void Update()
+        {
+            if(messageQueue.Count>0)
+            {
+                if(DateTime.UtcNow.Subtract(lastMessage).TotalMilliseconds>messageDelay)
+                {
+                    lastMessage = DateTime.UtcNow;
+                    Tuple<NGuid, string> message = messageQueue[0];
+                    messageQueue.RemoveAt(0);
+                    ChatManager.SendChatMessage(AddEnvelope(message.Item2, lengthenMessage), message.Item1);
+                }
+            }
+        }
+
+        public class Character
+        {
+            public bool NPC { get; set; } = false;
+            public List<Roll> attacks { get; set; } = new List<Roll>();
+            public List<Roll> skills { get; set; } = new List<Roll>();
+            public List<string> resistance { get; set; } = new List<string>();
+            public List<string> immunity { get; set; } = new List<string>();
+        }
+
+        public class Roll
+        {
+            public string name { get; set; } = "";
+            public string type { get; set; } = "";
+            public string roll { get; set; } = "1D20";
+            public Roll link { get; set; } = null;
+        }
+    }
+}
---
+++ TaleSpireDnD5EMacroPlugin/Handlers.cs
@@ -0,0 +1,216 @@
+﻿using BepInEx;
+using Bounce.Unmanaged;
+using System;
+using UnityEngine;
+
+namespace LordAshes
+{
+    public partial class DnD5EMacrosPlugin : BaseUnityPlugin
+    {
+        private System.Random random = new System.Random();
+        private int statForAC = 0;
+
+        private void StatChange(string[] names)
+        {
+            for(int i=0; i<names.Length; i++)
+            {
+                if (names[i] == "AC") { statForAC = i; Debug.Log("D&D 5e Macros Plug-In: Found AC at Index " + i); }
+            }
+        }
+
+        private bool CharacterCheck(string characterName, string rollName)
+        {
+            CreatureBoardAsset asset = null;
+            CreaturePresenter.TryGetAsset(LocalClient.SelectedCreatureId, out asset);
+            return (StatMessaging.GetCreatureName(asset) == characterName);
+        }
+
+        private void MenuSelection(CreatureGuid cid, Roll roll)
+        {
+            CreatureBoardAsset attacker = null;
+            CreatureBoardAsset victim = null;
+            CreaturePresenter.TryGetAsset(LocalClient.SelectedCreatureId, out attacker);
+            CreaturePresenter.TryGetAsset(new CreatureGuid(RadialUI.RadialUIPlugin.GetLastRadialTargetCreature()), out victim);
+
+            if(attacker!=null && victim!=null)
+            {
+                RollResult attackRoll = ExecuteRoll(roll.roll);
+                int victimAC = (int)victim.Creature.GetStatByIndex(statForAC).Max;
+                string attackMessage = roll.name + " " + attackRoll.total + "\r\n(" + attackRoll.expanded+")";
+                if (attackRoll.minRoll) { attackMessage = attackMessage + "\r\n(Critical Miss)"; }
+                if (attackRoll.maxRoll) { attackMessage = attackMessage + "\r\n(Critical Hit)"; }
+                messageQueue.Add(new Tuple<NGuid,string>(new NGuid(attacker.Creature.CreatureId.ToString()),attackMessage));
+                if ((attackRoll.total>=victimAC && !attackRoll.minRoll) || attackRoll.maxRoll)
+                {
+                    CreatureManager.AttackCreature(attacker.Creature.CreatureId, attacker.transform.position, victim.Creature.CreatureId, victim.transform.position);
+                    if (!characters[StatMessaging.GetCreatureName(victim)].NPC)
+                    {
+                        if (attackRoll.maxRoll)
+                        {
+                            if(characters[StatMessaging.GetCreatureName(victim)].immunity.Contains("Criticals"))
+                            {
+                                messageQueue.Add(new Tuple<NGuid, string>(new NGuid(victim.Creature.CreatureId.ToString()), "Critical Hit!\r\n(Immunity)\r\n(AC:" + victimAC + ")"));
+                                attackRoll.maxRoll = false;
+                            }
+                            else
+                            {
+                                messageQueue.Add(new Tuple<NGuid, string>(new NGuid(victim.Creature.CreatureId.ToString()), "Critical Hit!\r\n(AC:" + victimAC + ")"));
+                            }
+                        }
+                        else
+                        {
+                            messageQueue.Add(new Tuple<NGuid, string>(new NGuid(victim.Creature.CreatureId.ToString()), "Hit!\r\n(AC:" + victimAC + ")"));
+                        }
+                    } 
+                    else 
+                    {
+                        if (attackRoll.maxRoll)
+                        {
+                            if (characters[StatMessaging.GetCreatureName(victim)].immunity.Contains("Criticals"))
+                            {
+                                messageQueue.Add(new Tuple<NGuid, string>(new NGuid(victim.Creature.CreatureId.ToString()), "Critical Hit!\r\n(Immunity)"));
+                                attackRoll.maxRoll = false;
+                            }
+                            else
+                            {
+                                messageQueue.Add(new Tuple<NGuid, string>(new NGuid(victim.Creature.CreatureId.ToString()), "Critical Hit!"));
+                            }
+                        }
+                        else
+                        {
+                            messageQueue.Add(new Tuple<NGuid, string>(new NGuid(victim.Creature.CreatureId.ToString()), "Hit!"));
+                        }
+                    }
+                    RollResult attackerDamage = new RollResult();
+                    RollResult attackerDamageChat = new RollResult();
+                    RollResult victimDamage = new RollResult();
+                    RollResult victimDamageChat = new RollResult();
+                    while (roll.link!=null)
+                    {
+                        roll = roll.link;
+                        RollResult thisDamageRoll = ExecuteRoll(roll.roll, attackRoll.maxRoll);
+
+                        attackerDamage.total = attackerDamage.total + thisDamageRoll.total;
+                        attackerDamage.expanded = (attackerDamage.expanded == "") ? roll.name + ": " + thisDamageRoll.total + " " + roll.type : attackerDamage.expanded + "\r\n" + roll.name + ": " + thisDamageRoll.total + " " + roll.type;
+                        attackerDamageChat.expanded = (attackerDamageChat.expanded == "") ? thisDamageRoll.expanded : attackerDamageChat.expanded + ", " + thisDamageRoll.expanded;
+
+                        if (characters[StatMessaging.GetCreatureName(victim)].immunity.Contains(roll.type)) { thisDamageRoll.total = 0; thisDamageRoll.expanded = "("+thisDamageRoll.expanded+")x0"; }
+                        if (characters[StatMessaging.GetCreatureName(victim)].resistance.Contains(roll.type)) { thisDamageRoll.total = (int)Math.Ceiling((decimal)(thisDamageRoll.total / 2)); thisDamageRoll.expanded = "("+thisDamageRoll.expanded + ")x0.5"; }
+
+                        victimDamage.total = victimDamage.total + thisDamageRoll.total;
+                        victimDamage.expanded = (victimDamage.expanded == "") ? roll.name + ": " + thisDamageRoll.total + " " + roll.type  : victimDamage.expanded + "\r\n" + roll.name + ": " + thisDamageRoll.total + " " + roll.type;
+                        victimDamageChat.expanded = (victimDamageChat.expanded == "") ? thisDamageRoll.expanded : victimDamageChat.expanded + ", " + thisDamageRoll.expanded;
+                    }
+                    messageQueue.Add(new Tuple<NGuid, string>(new NGuid(attacker.Creature.CreatureId.ToString()), "Inflicts " + attackerDamage.total+" Damage\r\n\r\n"+attackerDamage.expanded));
+                    messageQueue.Add(new Tuple<NGuid, string>(NGuid.Empty, "<size="+expansionFontSize+">"+attackerDamageChat.expanded+"</size>"));
+                    int newHP = (int)(victim.Creature.Hp.Value - victimDamage.total);
+                    newHP = Math.Max(0, newHP);
+                    CreatureManager.SetCreatureStatByIndex(victim.Creature.CreatureId, new CreatureStat(newHP,victim.Creature.Hp.Max), -1);
+                    messageQueue.Add(new Tuple<NGuid, string>(new NGuid(victim.Creature.CreatureId.ToString()), "Receives "+victimDamage.total+" Damage\r\n\r\n"+victimDamage.expanded));
+                    messageQueue.Add(new Tuple<NGuid, string>(NGuid.Empty, "<size="+expansionFontSize+">"+victimDamageChat.expanded+"</size>"));
+                    if (!characters[StatMessaging.GetCreatureName(victim)].NPC)
+                    {
+                        if (newHP > 0)
+                        {
+                            messageQueue.Add(new Tuple<NGuid, string>(new NGuid(victim.Creature.CreatureId.ToString()), "At " + newHP + " of " + victim.Creature.Hp.Max + " HP"));
+                        }
+                        else
+                        {
+                            messageQueue.Add(new Tuple<NGuid, string>(new NGuid(victim.Creature.CreatureId.ToString()), "Unconcious and dying"));
+                        }
+                    }
+                }
+                else
+                {
+                    victim.Creature.PlayEmote("");
+                    if (!characters[StatMessaging.GetCreatureName(victim)].NPC)
+                    {
+                        messageQueue.Add(new Tuple<NGuid, string>(new NGuid(victim.Creature.CreatureId.ToString()), "Miss!\r\n(AC:" + victimAC + ")"));
+                    }
+                    else 
+                    {
+                        messageQueue.Add(new Tuple<NGuid, string>(new NGuid(victim.Creature.CreatureId.ToString()), "Miss!"));
+                    }
+                }
+            }
+        }
+
+        private RollResult ExecuteRoll(string rollSpecs, bool criticalHit = false)
+        {
+            string expanded = "";
+            string component1 = "";
+            string component2 = "";
+            string operation = "";
+            bool maxRoll = true;
+            bool minRoll = true;
+            while (rollSpecs != "")
+            {
+                component2 = "";
+                while ("0123456789".Contains(rollSpecs.Substring(0, 1)))
+                {
+                    component2 = component2 + rollSpecs.Substring(0, 1);
+                    rollSpecs = rollSpecs.Substring(1);
+                    if (rollSpecs == "") { break; }
+                }
+                switch (operation)
+                {
+                    case "D":
+                        expanded = expanded + component1 + "D" + component2 + "[";
+                        int total = 0;
+                        for (int d = 0; d < int.Parse(component1); d++)
+                        {
+                            int ran = random.Next(1, int.Parse(component2)+1);
+                            if (ran != 1) { minRoll = false; }
+                            if (ran != int.Parse(component2)) { maxRoll = false; }
+                            total = total + ((criticalHit) ? ran*2 : ran);
+                            expanded = expanded + ran + ",";
+                        }
+                        expanded = expanded.Substring(0, expanded.Length - 1) + "]"+((criticalHit)?"x2":"")+"=";
+                        component2 = total.ToString();
+                        break;
+                    case "+":
+                        expanded = expanded + component1 + "+" + component2 + "=";
+                        component2 = (int.Parse(component1) + int.Parse(component2)).ToString();
+                        break;
+                    case "-":
+                        expanded = expanded + component1 + "-" + component2 + "=";
+                        component2 = (int.Parse(component1) - int.Parse(component2)).ToString();
+                        break;
+                    case "*":
+                        expanded = expanded + component1 + "x" + component2 + "=";
+                        component2 = (int.Parse(component1) * int.Parse(component2)).ToString();
+                        break;
+                    case "/":
+                        expanded = expanded + component1 + "/" + component2 + "=";
+                        component2 = (int.Parse(component1) / int.Parse(component2)).ToString();
+                        break;
+                    default:
+                        break;
+                }
+                if (rollSpecs == "") { expanded = expanded.Substring(0, expanded.Length - 1); break; }
+                operation = rollSpecs.Substring(0, 1);
+                rollSpecs = rollSpecs.Substring(1);
+                component1 = component2;
+            }
+            return new RollResult() { total = int.Parse(component2), expanded = expanded, maxRoll = maxRoll, minRoll = minRoll };
+        }
+
+        public string AddEnvelope(string message, int envelopeCount)
+        {
+            string result = "";
+            for(int i=0; i<envelopeCount; i++)
+            {
+                result = result + "  ";
+            }
+            return (envelopeCount > 0) ? result + "\r\n" + message + "\r\n" + result : message;
+        }
+
+        public class RollResult
+        {
+            public int total { get; set; } = 0;
+            public string expanded { get; set; } = "";
+            public bool maxRoll = false;
+            public bool minRoll = false;
+        }
+    }
+}
